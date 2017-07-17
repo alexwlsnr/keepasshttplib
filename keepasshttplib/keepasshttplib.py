f06ab2edc4722f4f16260d0bff4ee6f421ebc907
Alex Wilson <alex.wilson@fasthosts.com>
2017-07-17 15:21:53 +0100

    Super rough kering code

--- keepasshttplib/keepasshttplib.py
+++ keepasshttplib/keepasshttplib.py
@@ -4,6 +4,7 @@ import requests
 import Crypto
 import Crypto.Random
 import os
+import keyring
 from Crypto.Cipher import AES
 from pkcs7 import PKCS7Encoder
 
@@ -13,7 +14,13 @@ PRIVATE_KEY = Crypto.Random.OSRNG.posix.new().read(AES.block_size)
 
 IV = os.urandom(16)
 
-BASE64_PRIVATE_KEY = base64.b64encode(PRIVATE_KEY).decode()
+keyring_key = keyring.get_password("keepasshttplib", "Key2")
+
+if(keyring_key == None):
+    BASE64_PRIVATE_KEY = base64.b64encode(PRIVATE_KEY).decode()
+else:
+    BASE64_PRIVATE_KEY = keyring_key
+    PRIVATE_KEY = base64.b64decode(keyring_key)
 
 BASE64_IV = base64.b64encode(IV).decode()
 
@@ -43,7 +50,13 @@ jsonreq = {
     "Verifier": VERIFIER,
 }
 
-r = requests.post('http://localhost:19455/post', json=jsonreq)
+r = requests.post('http://10.44.250.139:19455/post', json=jsonreq)
+
+json = r.json()
+
+if json['Success']:
+    keyring.set_password("keepasshttplib", "Id", json['Id'])
+    keyring.set_password("keepasshttplib", "Key2", BASE64_PRIVATE_KEY)
 
 print("Request: " + str(jsonreq))
 print("Status code: " + str(r.status_code))
